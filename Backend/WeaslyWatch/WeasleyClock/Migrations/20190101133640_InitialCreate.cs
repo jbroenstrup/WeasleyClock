e7d0be1ce1b5e8fe50870a2743f97d1630002272
Jonas-Home <Jonas.Broenstrup@gmail.com>
2019-01-01 14:38:35 +0100

    First steps with model first database creation.

---
+++ Backend/WeaslyWatch/WeasleyClock/Migrations/20190101133640_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WeasleyClock.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Latitude = table.Column<double>(nullable: false),
+                    Longitude = table.Column<double>(nullable: false),
+                    MaxDistance = table.Column<double>(nullable: false),
+                    SpeedMin = table.Column<double>(nullable: false),
+                    SpeedMax = table.Column<double>(nullable: false),
+                    LocationName = table.Column<string>(nullable: true),
+                    UserName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Locations");
+        }
+    }
+}
--- Backend/WeaslyWatch/WeasleyClock/Model/Data/Location.cs
+++ Backend/WeaslyWatch/WeasleyClock/Model/Data/Location.cs
@@ -7,10 +7,12 @@ namespace WeasleyClock.Model.Data
 {
     public class Location
     {
+        public Guid Id { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
         public double MaxDistance { get; set; }
-        public double[] SpeedRange { get; set; }
+        public double SpeedMin { get; set; }
+        public double SpeedMax { get; set; }
         public string LocationName { get; set; }
         public string UserName { get; set; }
     }
